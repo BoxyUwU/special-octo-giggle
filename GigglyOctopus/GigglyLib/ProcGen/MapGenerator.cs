bb08afdf7142565dc47d91e3c67efaf5a0772433
Jack Stanbridge <supbscripter@gmail.com>
2020-03-04 01:27:09 +0000

    tweaks

--- GigglyOctopus/GigglyLib/ProcGen/MapGenerator.cs
+++ GigglyOctopus/GigglyLib/ProcGen/MapGenerator.cs
@@ -12,16 +12,18 @@ namespace GigglyLib.ProcGen
 
         public void Generate()
         {
+            SpawnEnemy(3, -20, Direction.SOUTH);
+            SpawnEnemy(-4, -16, Direction.SOUTH);
             SpawnEnemy(5, 5);
             SpawnEnemy(10, 7);
-            SpawnEnemy(7, 2);
+            SpawnEnemy(-7, 2, Direction.EAST);
         }
 
-        public Entity SpawnEnemy(int gX, int gY)
+        public Entity SpawnEnemy(int gX, int gY, Direction dir = Direction.WEST)
         {
             var e = _world.CreateEntity();
             e.Set<CEnemy>();
-            e.Set(new CGridPosition { X = gX, Y = gY, Facing = Direction.WEST });
+            e.Set(new CGridPosition { X = gX, Y = gY, Facing = dir });
             e.Set<CMovable>();
             e.Set(new CHealth { Max = 15 });
             e.Set(Config.Weapons[new List<string>(Config.Weapons.Keys)[new Random().Next(0, Config.Weapons.Count)]]);
