a2bde56665020bcffee3d8bd3cabdd0a464609f7
Jack Stanbridge <supbscripter@gmail.com>
2020-03-07 22:47:50 +0000

    removed exit functionality

--- GigglyOctopus/GigglyLib/Game1.cs
+++ GigglyOctopus/GigglyLib/Game1.cs
@@ -380,9 +380,6 @@ namespace GigglyLib
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             /*var kS = Keyboard.GetState();
             if (kS.IsKeyDown(Keys.LeftShift) && kS.IsKeyDown(Keys.D))
             {
